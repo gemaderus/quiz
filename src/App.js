1320ff1a2834886cb01ccf980557ebda78f0c017
gemaderus <hi@gemaderus.com>
2018-04-20 15:03:20 +0200

    Added firebase.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,27 @@ import Home from './home';
 import Dashboard from './dashboard';
 import Ranking from './ranking';
 import './App.css';
+import * as firebase from 'firebase';
+import 'firebase/database';
+import flamelink from 'flamelink';
 
 class App extends Component {
   state = {
     path: 'home'
+  }
+
+  constructor(props) {
+    super()
+    var firebaseConfig = {
+      apiKey: "AIzaSyCxCI-dPd8qifWuKdLp32kLplI27smbUWg",
+      authDomain: "bbva-2721a.firebaseapp.com",
+      databaseURL: "https://bbva-2721a.firebaseio.com",
+      projectId: "bbva-2721a",
+      storageBucket: "bbva-2721a.appspot.com",
+      messagingSenderId: "146691531247"
+    };
+    const firebaseApp = firebase.initializeApp(firebaseConfig);
+    const app = flamelink({ firebaseApp });
   }
 
   selectPath = (path) =>{
